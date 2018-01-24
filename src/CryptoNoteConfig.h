dcd64501afa1ccf49f6be3a0104f4d2f208d08ac
mealhalabi <35702597+mealhalabi@users.noreply.github.com>
2018-01-24 21:10:12 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -110,7 +110,7 @@ namespace parameters {
 
 
 const char     CRYPTONOTE_NAME[]                             = "Cybercoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "010a01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101537b6d3c571ce978de83777bde8576793e78a39c7cbaf9f0e45f10af301868c1";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "010a01ff0001904e029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121011747e1c4c9c7b0b25a7387fa6f1a8714d87cdb8dfc30f21c180ebe374c6bf1b0";
 const uint32_t GENESIS_NONCE                                 = 70;
 const uint64_t GENESIS_TIMESTAMP                             = 1515479625;
 
